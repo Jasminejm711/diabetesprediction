914579d9c1ab5afaab2400446975a61433ee88b1
Jasminejm711 <lowjm-wm22@student.tarc.edu.my>
2024-09-12 06:02:52 +0800

    Update diabetesPrediction.py

--- diabetesPrediction.py
+++ diabetesPrediction.py
@@ -73,7 +73,7 @@ def main():
         age_input = st.number_input("Enter Age", min_value=0)
         hypertension_input = st.radio("Hypertension (No=0, Yes=1)", (0, 1))
         heart_disease_input = st.radio("Heart Disease (No=0, Yes=1)", (0, 1))
-        smoking_history_input = st.selectbox("Select Smoking History", ["current", "ever", "former", "never"])
+        smoking_history_input = st.selectbox("Select Smoking History", ["current", "ever", "former", "never","not cureent"])
         bmi_input = st.number_input("Enter BMI", format="%.2f")
         HbA1c_level_input = st.number_input("Enter HbA1c Level", format="%.2f")
         blood_glucose_level_input = st.number_input("Enter Blood Glucose Level", format="%.2f")
