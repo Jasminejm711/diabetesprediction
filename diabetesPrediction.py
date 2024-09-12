337c9bae9e3c56d3e371b3f050ae1f1b20d95eec
Jasminejm711 <lowjm-wm22@student.tarc.edu.my>
2024-09-12 15:08:42 +0800

    Update diabetesPrediction.py

--- diabetesPrediction.py
+++ diabetesPrediction.py
@@ -74,9 +74,9 @@ def main():
         hypertension_input = st.radio("Hypertension (No=0, Yes=1)", (0, 1))
         heart_disease_input = st.radio("Heart Disease (No=0, Yes=1)", (0, 1))
         smoking_history_input = st.selectbox("Select Smoking History", ["current", "ever", "former", "never", "not current"])
-        bmi_input = st.number_input("Enter BMI", format="%.2f")
-        HbA1c_level_input = st.number_input("Enter HbA1c Level", format="%.2f")
-        blood_glucose_level_input = st.number_input("Enter Blood Glucose Level", format="%.2f")
+        bmi_input = st.number_input("Enter BMI", format="%.2f", min_value=0.01, step=0.01)
+        HbA1c_level_input = st.number_input("Enter HbA1c Level", format="%.2f", min_value=0.01, step=0.01)
+        blood_glucose_level_input = st.number_input("Enter Blood Glucose Level", format="%.2f", min_value=0.01, step=0.01)
 
         # Predict button
         if st.button('Predict'):
